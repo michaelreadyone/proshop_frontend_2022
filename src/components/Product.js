90db93ce8b44780bbd4e757ee3a19449bc21de8c
Mike <michael56653556@gmail.com>
2022-02-09 02:21:39 -0500

    S2L8 Rating Component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 function Product({ product }) {
   return (
@@ -17,6 +18,11 @@ function Product({ product }) {
         <Card.Text as="div">
           <div className="my-3">
             {product.rating} from {product.numReviews} reviews
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+              color="#f8e825"
+            />
           </div>
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
